0296034bced570d2d947a96c264c765659465018
Bainbridge <jack.bainbridge@medavie.bluecross.ca>
2022-09-30 13:27:29 -0300

    Adding Vet Controller and list of vets thymeleaf. Closes #6

--- pet-clinic-web/src/main/java/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/petclinic/PetClinicApplication.java
@@ -18,6 +18,7 @@ package petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * PetClinic Spring Boot Application.
