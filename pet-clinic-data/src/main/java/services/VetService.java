f6326ef0f14ed10a77d82be467968ec15bf6931f
Bainbridge <jack.bainbridge@medavie.bluecross.ca>
2022-10-03 09:55:45 -0300

    Adding Bootstrap start up data for the application - owners, vets

--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -4,6 +4,4 @@ import model.Vet;
 
 public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findByLastName(String lastName);
-
 }
---
+++ pet-clinic-web/src/main/java/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,62 @@
+package petclinic.bootstrap;
+
+import model.Owner;
+import model.Vet;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import services.OwnerService;
+import services.VetService;
+import services.map.OwnerServiceMap;
+import services.map.VetServiceMap;
+
+/**
+ * Spring boot specific CommandLineRunner to load data into out application on start up.
+ * When Spring is up and ready, commandLineRunner is then executed.
+ */
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        vetService = new VetServiceMap();
+        ownerService = new OwnerServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Jack");
+        owner1.setLastName("Bainbridge");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Testing");
+        owner2.setLastName("Second");
+
+        ownerService.save(owner2);
+
+        System.out.println("Owners have been loaded...");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Testing");
+        vet2.setLastName("SecondVet");
+
+        vetService.save(vet2);
+
+        System.out.println("Vets have been loaded...");
+    }
+}
